45250d821d6bd688aa595982c63656e8abf1071e
Ashley <yixin.xu2@mail.mcgill.ca>
2025-03-20 20:06:33 -0400

    Naive approach without RAG or Finetuning (#2)
    * Naive approach without RAG or Finetuning
    
    * correction based on PR review
    
    * removed extract.py
    
    ---------
    
    Co-authored-by: Ashley Xu <ashleyxu@Ashleys-MBP.lan>
    Co-authored-by: Ashley Xu <ashleyxu@Ashleys-MacBook-Pro.local>

--- scripts/naive/model.py
+++ scripts/naive/model.py
@@ -1,1 +1,36 @@
 # Naive approach using a foundation model with no RAG or fine tuning
+from openai import OpenAI
+from openai.types.chat import ChatCompletionMessageParam
+
+openai_api_client = OpenAI()
+
+def generation(prompt_messages: list[ChatCompletionMessageParam]) -> str:
+    response = openai_api_client.chat.completions.create(
+        messages=prompt_messages,
+        model='gpt-4o-mini',
+        temperature=0,
+    )
+
+    return response.choices[0].message.content
+
+def main():
+    # Example prompt messages
+    example_prompt = [
+        {
+            "role": "system",
+            "content": "You are a therapist specialized in the cognitive behavioral therapy."
+        },
+        {
+            "role": "user",
+            "content": "It's been years since I have had nightmares about my father. Whenever I talk to him or even just think about him, I get a knod in my stomach and gets anxious. But if I stop talking him, I also feels off and not grounded. What should I do? "
+        }
+    ]
+    try:
+        response = generation(example_prompt)
+        print("Assistant's response:")
+        print(response)
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+if __name__=='__main__':
+    main()
